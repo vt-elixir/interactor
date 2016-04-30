0f83f5fb622c8be37af8be4a7077ec6f4cb9da28
Alan Peabody <gapeabody@gmail.com>
2016-04-30 11:04:17 -0400

    Better hooks.

--- lib/interactor/perform.ex
+++ lib/interactor/perform.ex
@@ -1,15 +1,5 @@
 defmodule Interactor.Perform do
-  def perform(interactor, input, nil) do
-    do_perform(interactor, input, nil)
-  end
-
   def perform(interactor, input, repo) do
-    repo.transaction fn ->
-      do_perform(interactor, input, repo)
-    end
-  end
-
-  defp do_perform(interactor, input, repo) do
     input
     |> interactor.before_call
     |> interactor.call
